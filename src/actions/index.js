dfb180e27a1fa246de9a5237a3d2aca8a3ad5481
MatheStine <cmturner072@gmail.com>
2021-02-13 16:37:30 -0600

    Fully Functional results page

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import { SET_RECENT_POSTS } from './types';
+import { SET_RECENT_POSTS, SET_RESULTS_POSTS } from './types';
 
 import axios from 'axios';
 
@@ -14,5 +14,18 @@ export function fetchRecentPosts() {
                 payload: response.data.posts
             })
         })
+    }
+}
+
+export function fetchPostsWithQuery(query) {
+    return function(dispatch) {
+        axios.get(`https://api.dailysmarty.com/search?q=${query}`)
+            .then(response => {
+                console.log(response.data.posts);
+                dispatch({
+                    type: SET_RESULTS_POSTS,
+                    payload: response.data.posts
+                })
+            })
     }
 }
--- src/components/results.js
+++ src/components/results.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Logo from './logo';
 import SearchBar from './searchBar';
+import ResultsPosts from './resultsPosts';
+
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class Results extends Component {
 
   handleSearchBarSubmit(query) {
-    console.log(query);
+    this.props.fetchPostsWithQuery(query);
   }
 
 
@@ -14,8 +18,9 @@ class Results extends Component {
       <div>
         <Logo size={55}/>
         <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
+        <ResultsPosts />
       </div>
     )
   }
 }
-export default Results;
+export default connect(null, actions)(Results);
